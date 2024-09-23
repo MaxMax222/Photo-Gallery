eaca99653e1c150547b9806f7e2ea5f76ac7c764
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-09-23 10:31:17 +0300

    added grid mode functionality

--- Photo-Gallery/Grid.cs
+++ Photo-Gallery/Grid.cs
@@ -16,13 +16,54 @@ namespace Photo_Gallery
 	[Activity (Label = "Grid")]			
 	public class Grid : Activity
 	{
+		LinearLayout main;
+		Random rnd = new Random();
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
 			// Create your application here
 			SetContentView(Resource.Layout.Grid);
+			main = FindViewById<LinearLayout>(Resource.Id.mainLayout);
+            GenerateGrid(); 
+
 		}
-	}
+
+		void GenerateGrid()
+		{
+            string v = Intent.GetStringExtra("amount");
+            int imgToShow = v == "" ? 0 : int.Parse(v);
+            char[] types = { 's', 'c', 'h', 'd' };
+            LinearLayout layout = new LinearLayout(this);
+            for (int i = 0; i <= imgToShow; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    if (i > 0)
+                    {
+                        var img = new ImageView(this);
+                        int num1 = rnd.Next(1, 14);
+                        char type1 = types[rnd.Next(4)];
+                        img.SetImageResource(Resources.GetIdentifier($"img{num1}{type1}", "drawable", this.PackageName));
+                        img.LayoutParameters = new LinearLayout.LayoutParams(0, 400, 1);
+                        layout.AddView(img);
+                    }
+                    main.AddView(layout);
+                    layout = new LinearLayout(this);
+                    continue;
+                }
+                //add image
+                var imgv = new ImageView(this);
+                int num = rnd.Next(1, 14);
+                char type = types[rnd.Next(4)];
+                imgv.SetImageResource(base.Resources.GetIdentifier($"img{num}{type}", "drawable", this.PackageName));
+                imgv.LayoutParameters = new LinearLayout.LayoutParams(0, 400, 1);
+                layout.AddView(imgv);
+            }
+
+            main.AddView(layout);
+
+        }
+    }
 }
 
